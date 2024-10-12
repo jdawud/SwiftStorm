40fdf517b925906f545bb42110e69be8798ba29e
jdawud <jdawud@gmail.com>
2024-10-12 08:03:16 -0600

    Initial Commit

---
+++ SwiftStorm/SwiftStormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStormApp.swift
+//  SwiftStorm
+//
+//  Created by Junaid Dawud on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
